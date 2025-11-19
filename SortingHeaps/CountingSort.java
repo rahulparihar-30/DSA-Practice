7b729606e650f035cd2f78fb90830a5a92d56415
rahulaauji-30 <rahulaauji71@gmail.com>
2025-11-20 00:33:03 +0530

    implment Counting sort

--- SortingHeaps/CountingSort.java
+++ SortingHeaps/CountingSort.java
@@ -11,11 +11,11 @@ public class CountingSort {
     }
 
     public void countingSort(int[] nums,int n){
-        int maxx,k;
+        int maxx,k,c;
         int[] frequency;
 
         //1. Calculate the max element from the array.
-        maxx = max(nums,n);
+        maxx = max(nums,n); // n
 
         //2. initialize the array with the size of the max element
         frequency = new int[maxx+1];
@@ -25,14 +25,28 @@ public class CountingSort {
         for (int i = 0;i<n;i++){
             frequency[nums[i]]+=1;
         }
+        // n
 
         //4. Copy back the in to original array;
          k = 0;
+        /*
         for (int i=0;i<maxx+1;i++){
             while (frequency[i]!=0){
                 nums[k++] = i;
                 frequency[i]-=1;
             }
         }
+        Time Complexity = O(n+k)
+        */
+        c = 0;
+        while (c < maxx+1){
+            if (frequency[c] > 0){
+                nums[k++] = c;
+                frequency[c]--;
+            }else {
+                c++;
+            }
+        }
+//        Time Complexity = O(n)
     }
 }
